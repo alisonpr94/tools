5a2bff1724bb19f83c21268c911531d82faddbc5
Alison Pereira Ribeiro <alisonrib17@gmail.com>
2023-11-15 11:11:23 -0300

    Adicionando endpoint de atualização

--- tools/urls.py
+++ tools/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ToolListCreateView, ToolDeleteView, ToolRetrieveView, ToolListByTagView
+from .views import ToolListCreateView, ToolDeleteView, ToolRetrieveView, ToolListByTagView, ToolRetrieveUpdateView
 
 urlpatterns = [
     path('tools/', ToolListCreateView.as_view(), name='tool-list-create'),
     path('tools/<int:pk>/', ToolRetrieveView.as_view(), name='tool-retrieve'),
     path('tools/<int:pk>/delete/', ToolDeleteView.as_view(), name='tool-delete'),
     path('tools/by-tag/<str:tag>/', ToolListByTagView.as_view(), name='tool-list-by-tag'),
+    path('tools/<int:pk>/update/', ToolRetrieveUpdateView.as_view(), name='tool-update'),
 ]
--- tools/views.py
+++ tools/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from .models import Tool
 from .serializers import ToolSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 class ToolListCreateView(generics.ListCreateAPIView):
     queryset = Tool.objects.all()
@@ -32,3 +34,22 @@ class ToolListByTagView(generics.ListAPIView):
         if tag:
             return Tool.objects.filter(tags__contains=[tag])
         return Tool.objects.all()
+    
+class ToolRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Tool.objects.all()
+    serializer_class = ToolSerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        tags = request.data.get('tags', None)
+
+        if tags:
+            instance.tags = tags
+            instance.save()
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
